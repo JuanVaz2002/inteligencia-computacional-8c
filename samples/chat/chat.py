be78edaed5448147469f4308c4447e044de92355
JuanVaz2002 <juanfrancisco.vazquez@cetys.edu.mx>
2026-05-13 02:20:43 -0700

    chat.py subido

---
+++ samples/chat/chat.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+# from dotenv import load_dotenv        # No se requiere dotenv
+
+SYSTEM_MESSAGE = "You are a chatbot. You will have a conversation with a user. Be friendly and concise"
+
+if __name__ == "__main__":
+    URL = "http://127.0.0.1:1234/v1"    # El enlace del servidor local de LM Studio
+    KEY = "."                           # La llave API NO se requiere por LM Studio (se ignora) 
+    MODEL = "google/gemma-4-e4b"        # El nombre completo del modelo LLM local para que se seleccione.
+
+    client = OpenAI(
+        base_url=URL,
+        api_key=KEY,
+    )
+
+    print(f"Chatting with {MODEL} model at {URL}\n")
+
+    while True:
+        message = input("> ")
+        response = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {'role': 'system', 'content': SYSTEM_MESSAGE},
+                {'role': 'user', 'content': message},
+            ]
+        )
+        print(response.choices[0].message.content)
